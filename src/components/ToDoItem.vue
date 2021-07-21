93de9ba6d9abb56659396fc19dc99a8a2bfafe6a
Anoj Joseph <anojjoseph1@gmail.com>
2021-07-21 15:40:26 +0530

    Started Vue.js learning. Uploading raw components.

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="checkbox" :id="id" :checked="IsDone" />
+        <label :for="id">{{ label }}</label>
+    </div>
+</template>
+<script>
+    import uniqueId from 'lodash.uniqueid';
+    export default{
+        props:{
+            label:{required: true, type: String},
+            done:{default: false, type: Boolean}
+        },
+        data(){
+            return{
+                IsDone :this.done,
+                id: uniqueId('todo-')
+            };
+        }
+    };
+</script>
