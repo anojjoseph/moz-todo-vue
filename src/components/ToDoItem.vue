0eb73afb3da9b1bddc7a3fcd8e669eae2d8b5d02
Anoj Joseph <anojjoseph1@gmail.com>
2021-07-24 13:19:08 +0530

    Edit enabled for todo items

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,25 +1,71 @@
 <template>
+  <div class="stack-small" v-if="!isEditing">
     <div class="custom-checkbox">
-        <input type="checkbox" :id="id" :checked="IsDone" class="checkbox" />
-        <label :for="id" class="checkbox-label">{{ label }}</label>
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')"
+      />
+      <label :for="id" class="checkbox-label">{{ label }}</label>
     </div>
+    <div class="btn-group">
+      <button type="button" class="btn" @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{ label }}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{ label }}</span>
+      </button>
+    </div>
+  </div>
+  <to-do-item-edit-form v-else :id="id" :label="label" 
+          @item-edited="itemEdited"
+          @edit-cancelled="editCancelled"
+  ></to-do-item-edit-form>
 </template>
 <script>
-    export default{
-        props:{
-            label:{required: true, type: String},
-            done:{default: false, type: Boolean},
-            id:{required: true, type: String}
-        },
-        data(){
-            return{
-                IsDone :this.done,
-            };
-        }
+import ToDoItemEditForm from "./ToDoItemEditForm.vue";
+export default {
+  components: {
+    ToDoItemEditForm
+  },
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+    id: { required: true, type: String },
+  },
+  data() {
+    return {
+      isEditing: false,
     };
+  },
+  computed:{
+    isDone(){
+      return this.done;
+    }
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+
+    toggleToItemEditForm() {
+      console.log('edit clicked');
+      this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit('item-edited', newLabel);
+      this.isEditing = false;
+    },
+    editCancelled(){
+      this.isEditing = false;
+    }
+  }
+};
 </script>
 <style scoped>
-    .custom-checkbox > .checkbox-label {
+.custom-checkbox > .checkbox-label {
   font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
